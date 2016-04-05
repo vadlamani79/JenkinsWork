79af80418fc31cc131449736d491c68b5e7d7517
Mahesh <vumamah@gmail.com>
2016-04-05 10:16:17 -0400

    new file

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,10 @@ package com.mahi;
 class Hello{
 
 public void sayHi(){
-	System.out.println("Hello World");
+	System.out.println("Hello World git to jenkins");
 	}
+
+public static void main(String args[]){
+	new Hello().sayHi();
+}
 }
